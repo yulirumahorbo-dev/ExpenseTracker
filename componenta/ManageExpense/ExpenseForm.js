6e19860409e77edebbf94eafdf1542c1ff642f4e
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-26 14:18:56 +0700

    perf: Setting & using default values

--- componenta/ManageExpense/ExpenseForm.js
+++ componenta/ManageExpense/ExpenseForm.js
@@ -2,12 +2,13 @@ import { StyleSheet, View, Text } from "react-native";
 import Input from "./Input";
 import { useState } from "react";
 import Button from "../UI/Button";
+import { getFormattedDate } from "../../util/date";
 
-function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit, defaultValues }) {
   const [inputValues, setInputValues] = useState({
-    amount: "",
-    date: "",
-    description: "",
+    amount: defaultValues ? defaultValues.amount.toString() : "",
+    date: defaultValues ? getFormattedDate(defaultValues.date) : "",
+    description: defaultValues ? defaultValues.description : "",
   });
 
   function inputChangeHandler(inputIdentifier, enteredValue) {
