6532412a94da3f415335d18bc68c01061c02e6de
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-27 13:48:18 +0700

    perf: Fetching backend data (get requests)

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpensesOutput from "../componenta/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpense } from "../util/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpense();
+    }
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
